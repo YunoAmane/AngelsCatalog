f3da7ebd871b3524b1bf271a8b3b3e7d5c72bcd1
Yuno Amane <yoppi_tuffy@gmail.com>
2018-08-17 11:29:56 -0500

    Commit inicial

---
+++ AngelsCatalog/src/Modelo/ConexionBD.java
@@ -0,0 +1,13 @@
+package Modelo;
+
+/**
+ * @author Henry
+ * @date 17-ago-2018
+ * @time 11:20:07
+ */
+public class ConexionBD {
+    public static void main(String[] args) {
+        System.out.println("Holi Angels Catalog :)");
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+#AngelsCatalog: Catalogo de descuentos
